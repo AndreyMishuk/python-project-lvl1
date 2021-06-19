ea468b512c254197522405cfd5e3cadf5d7a2488
AndreyMishuk <andreygim.am@gmail.com>
2021-06-19 17:52:34 +0300

    remove print()

--- brain_games/cli.py
+++ brain_games/cli.py
@@ -3,8 +3,5 @@
 import prompt
 
 
-def welcome_user():
-    print('Welcome to the Brain Games!')
-    name = prompt.string('May I have your name? ')
-
-    return name
+def welcome_user(string):
+    return prompt.string(string)
